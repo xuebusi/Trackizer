58a680cbf38334045f66247f5e6c679b5aecab89
xuebusi <xuebusi@qq.com>
2023-10-19 11:41:17 +0800

    init

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,22 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by CodeForAny on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+           
+            NavigationView {
+                MainTabView()
+            }
+            
+            
+        }
+    }
+}
